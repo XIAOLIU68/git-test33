d1e405d0fad44856148edc03e310f75d305b7615
ly <ly@qq.com>
2022-07-01 15:53:01 +0800

    master 3

--- src/main/java/com/liu/GitTest.java
+++ src/main/java/com/liu/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git1!");
         System.out.println("hello git2!");
         System.out.println("hello git6666!");
+        System.out.println("master git6666!");
     }
 }

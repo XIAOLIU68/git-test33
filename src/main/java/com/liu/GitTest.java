fd0c5e7e06625c49409db4778f901b874a243549
ly <ly@qq.com>
2022-07-01 15:40:48 +0800

    first commit

---
+++ src/main/java/com/liu/GitTest.java
@@ -0,0 +1,11 @@
+package com.liu;
+
+/**
+ * Created 小刘  on 创建时间2022/7/1  15:37
+ * 祝您永无BUG!
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}

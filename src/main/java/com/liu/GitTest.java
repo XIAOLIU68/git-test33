eca6326c37d4796b5c7e47f4808c0b490a682367
ly <ly@qq.com>
2022-07-01 15:42:59 +0800

    2 commit

--- src/main/java/com/liu/GitTest.java
+++ src/main/java/com/liu/GitTest.java
@@ -6,6 +6,8 @@ package com.liu;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
